f054bb26a6d32469a04cbcc8567593b6ad06c26d
KaitlynBro <kaitlynbrouwers@gmail.com>
2019-07-08 21:37:11 -0400

    skeleton done...v2 will be making it much more dry

--- src/Components/Health.js
+++ src/Components/Health.js
@@ -7,7 +7,35 @@ class Health extends React.Component {
                 <h2>Don't Forget about your <span>Health</span></h2>
                 <div>
                     <div>
-                        
+                        <h3>Water Intake</h3>
+                        <ul>
+                            <input type='checkbox' />
+                            <input type='checkbox' />
+                            <input type='checkbox' />
+                            <input type='checkbox' />
+                            <input type='checkbox' />
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>Today's workout</h3>
+                        <textarea rows="4" cols="50"> </textarea>                        
+                    </div>
+                    <div>
+                        <h3>Meals</h3>
+                        <div>
+                            <div>
+                                <h4>Breakfast</h4>
+                                <textarea rows="4" cols="50"> </textarea>
+                            </div>
+                            <div>
+                                <h4>Lunch</h4>
+                                <textarea rows="4" cols="50"> </textarea>
+                            </div>
+                            <div>
+                                <h4>Dinner</h4>
+                                <textarea rows="4" cols="50"> </textarea>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
